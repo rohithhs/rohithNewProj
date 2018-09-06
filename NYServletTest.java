69cd8ae46522408507bd68d38421f31ee75aaa42
rohithhs <rohithhs@gmail.com>
2018-09-07 00:01:30 +0530

    Test Class for servlet

---
+++ NYServletTest.java
@@ -0,0 +1,28 @@
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.junit.Test;
+
+public class NYServletTest{
+	
+	@Test
+    public void testServlet() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);       
+        HttpServletResponse response = mock(HttpServletResponse.class);    
+
+        when(request.getParameter("username")).thenReturn("me");
+        when(request.getParameter("password")).thenReturn("secret");
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        new NYServlet().doPost(request, response)	;
+
+        verify(request, atLeast(1)).getParameter("username"); // only if you want to verify username was called...
+        writer.flush(); // it may not have been flushed yet...
+        assertTrue(stringWriter.toString().contains("My expected string"));
+    }
+}
+
+}
